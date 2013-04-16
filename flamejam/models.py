632e085a4d81607bbc6288eb6758e7d9a002df15
Paul Bienkowski <opatutlol@aol.com>
2013-04-16 08:06:39 +0200

    Restyled again

--- flamejam/models.py
+++ flamejam/models.py
@@ -359,11 +359,13 @@ class Registration(db.Model):
     jam_id = db.Column(db.Integer, db.ForeignKey("jam.id"))
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
     show_in_finder = db.Column(db.Boolean, default = True)
+    registered = db.Column(db.DateTime)
 
     def __init__(self, user, jam, show_in_finder = True):
         self.user = user
         self.jam = jam
         self.show_in_finder = show_in_finder
+        self.registered = datetime.utcnow()
 
 class DevlogPost(db.Model):
     id = db.Column(db.Integer, primary_key = True)
