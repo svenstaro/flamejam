4cef305df0e88792f3d155b927c75711f434c6bd
Sven-Hendrik Haase <sh@lutzhaase.com>
2013-05-01 06:20:05 +0200

    Convert password

--- flamejam/models/user.py
+++ flamejam/models/user.py
@@ -77,7 +77,7 @@ class User(db.Model):
         # combine a few properties, hash it
         # take first 16 chars for simplicity
         # make it email specific
-        hash = scrypt.hash(self.username + self.new_email + self.password, app.config['SECRET_KEY'])
+        hash = scrypt.hash(self.username + self.new_email + str(self.password), app.config['SECRET_KEY'])
         return hash.encode('hex')[:16]
 
     def getResetToken(self):
