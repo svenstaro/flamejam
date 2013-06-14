5b55441991c7334a198715ab5b518666a2d2fd01
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2013-06-14 03:18:42 +0200

    Changed user-registration relationship to joined loading behavior

--- flamejam/models/user.py
+++ flamejam/models/user.py
@@ -22,7 +22,7 @@ class User(db.Model):
     ratings = db.relationship('Rating', backref='user', lazy = "dynamic")
     comments = db.relationship('Comment', backref='user', lazy = "dynamic")
     invitations = db.relationship("Invitation", backref = "user", lazy = "dynamic")
-    registrations = db.relationship("Registration", backref = "user", lazy = "dynamic")
+    registrations = db.relationship("Registration", backref = "user", lazy = "joined")
 
     ability_programmer = db.Column(db.Boolean)
     ability_gamedesigner = db.Column(db.Boolean)
